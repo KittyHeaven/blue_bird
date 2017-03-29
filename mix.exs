498dffe079b0bfc5d43e2701f9bfaf8475767d49
Djordje Atlialp <djordje.atlialp@grandcentrix.net>
2017-03-30 00:40:22 +0200

    Improve mix.ex

--- mix.exs
+++ mix.exs
@@ -5,8 +5,10 @@ defmodule BlueBird.Mixfile do
     [
       app: :blue_bird,
       version: "0.1.0",
-      elixir: "~> 1.3",
-      description: "BlueBird generates API documentation from annotations in controllers actions and tests cases.",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
       package: package(),
       deps: deps()
     ]
@@ -25,23 +27,37 @@ defmodule BlueBird.Mixfile do
     ]
   end
 
-  # Should work with all versions
   defp deps do
     [
       # Static code analysis
       {:credo, "~> 0.7.2", only: :dev},
+
+      # Docs
+      {:ex_doc, ">= 0.15.0", only: :dev},
+
+      # Composable modules
       {:plug, ">= 1.3.0"},
-      {:poison, ">= 3.0.0"},
-      {:ex_doc, ">= 0.15.0", only: :dev}
+
+      # JSON library
+      {:poison, ">= 3.0.0"}
     ]
+  end
+
+  defp description do
+    """
+    BlueBird generates API documentation from annotations in controllers actions and tests cases.
+    """
   end
 
   defp package do
     [
-      files: ["lib", "mix.exs", "README.md"],
-      contributors: ["Paul Smoczyk", "Djordje Atlialp"],
+      name: :blue_bird,
+      files: ["lib", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+      maintainers: ["Paul Smoczyk", "Djordje Atlialp"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/rhazdon/blue_bird"}
+      links: %{
+        "GitHub" => "https://github.com/rhazdon/blue_bird"
+      }
     ]
   end
 end
