cfb41459f707b4bcb719f3ac0853de84b0c43ae1
Djordje Ilic <rhazdon@gmail.com>
2017-03-30 02:22:55 +0200

    Add tests for conn_logger

---
+++ test/conn_logger_test.exs
@@ -0,0 +1,26 @@
+defmodule ConnLoggerTest do
+  use ExUnit.Case
+  doctest BlueBird
+
+  test "save/1" do
+    BlueBird.ConnLogger.reset()
+    BlueBird.ConnLogger.save(get_test_conn())
+
+    assert BlueBird.ConnLogger.conns() == [get_test_conn()]
+  end
+
+  test "conns/0" do
+    BlueBird.ConnLogger.reset()
+
+    assert BlueBird.ConnLogger.conns() == []
+
+    BlueBird.ConnLogger.save(get_test_conn())
+    BlueBird.ConnLogger.save(get_test_conn())
+
+    assert BlueBird.ConnLogger.conns() == [get_test_conn(), get_test_conn()]
+  end
+
+  defp get_test_conn do
+    %{test: "test"}
+  end
+end
