75435aeae93e38b867a768c6f33c6b61e3f2f6b5
Djordje Ilic <rhazdon@gmail.com>
2017-04-01 22:52:28 +0200

    Add TestRouter and TestController to test_helper.exs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,65 @@
+defmodule TestRouter do
+  @moduledoc """
+  `TestRouter` simulates a Phoenix Framework router.
+  """
+  use Phoenix.Router
+  import Plug.Conn
+  import Phoenix.Controller
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/" do
+    pipe_through :api
+
+    get     "/get",       TestController, :get
+    post    "/post",      TestController, :post
+    put     "/put",       TestController, :put
+    patch   "/patch",     TestController, :patch
+    delete  "/delete",    TestController, :delete
+  end
+end
+
+defmodule TestController do
+  @moduledoc """
+  `TestController` simulates a Phoenix Framework controller.
+  """
+  use Phoenix.Controller
+  use BlueBird.Controller
+
+  @ok Poison.encode!(%{status: "ok"})
+
+  api :GET, "/get" do
+    group "Test"
+    title "Test GET"
+  end
+  def get(conn, _params), do: send_resp(conn, 200, @ok)
+
+  api :POST, "/post" do
+    group "Test"
+    title "Test POST"
+  end
+  def post(conn, _params), do: send_resp(conn, 201, @ok)
+
+  api :PUT, "/put" do
+    group "Test"
+    title "Test PUT"
+  end
+  def put(conn, _params), do: send_resp(conn, 201, @ok)
+
+  api :PATCH, "/patch" do
+    group "Test"
+    title "Test PATCH"
+  end
+  def patch(conn, _params), do: send_resp(conn, 201, @ok)
+
+  api :DELETE, "/delete" do
+    group "Test"
+    title "Test DELETE"
+  end
+  def delete(conn, _params), do: send_resp(conn, 204, @ok)
+end
+
+# Start ExUnit
 ExUnit.start()
