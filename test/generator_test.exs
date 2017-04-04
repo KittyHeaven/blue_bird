7ef8c792879ff9c275b6fa28a0491d7477333d94
Djordje Ilic <rhazdon@gmail.com>
2017-04-04 17:49:37 +0200

    Add more tests for generator

--- test/generator_test.exs
+++ test/generator_test.exs
@@ -222,4 +222,268 @@ defmodule GeneratorTest do
       title: "API Documentation"
     }
   end
+
+  test "generator run with PUT" do
+    BlueBird.ConnLogger.reset()
+
+    # Create a test connection
+    conn = conn(:put, "/put", %{p: 5})
+    |> put_req_header("content-type", "application/json")
+
+    # Invoke the plug
+    conn = TestRouter.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 201
+
+    BlueBird.ConnLogger.save(conn)
+
+    assert BlueBird.Generator.run() ==
+    %{description: "Enter API description in mix.exs - blue_bird_info",
+      host: "http://localhost",
+      routes: [
+        %{description: nil,
+          group: "Test",
+          method: "GET",
+          note: nil,
+          parameters: [],
+          path: "/get",
+          requests: [],
+          title: "Test GET"
+        },
+        %{description: nil,
+          group: "Test",
+          method: "POST",
+          note: "This is a note",
+          parameters: [],
+          path: "/post",
+          requests: [],
+          title: "Test POST"
+        },
+        %{description: nil,
+          group: "Test",
+          method: "PUT",
+          note: nil,
+          parameters: [],
+          path: "/put",
+          requests: [
+            %{headers: [{"content-type", "application/json"}],
+              method: "PUT",
+              params: "{\"p\":5}",
+              path: "/put",
+              path_params: %{},
+              response: %{
+                body: "{\"status\":\"ok\"}",
+                headers: [{"cache-control", "max-age=0, private, must-revalidate"}],
+                status: 201
+              }
+            }
+          ],
+          title: "Test PUT"
+        },
+        %{description: nil,
+          group: "Test",
+          method: "PATCH",
+          note: nil,
+          parameters: [
+            %{description: "Post ID or slug",
+              name: "post_id",
+              required: true,
+              type: "integer"
+            }
+          ],
+          path: "/patch",
+          requests: [],
+          title: "Test PATCH"
+        },
+        %{description: nil,
+          group: "Test",
+          method: "DELETE",
+          note: nil,
+          parameters: [],
+          path: "/delete",
+          requests: [],
+          title: "Test DELETE"
+        }
+      ],
+      title: "API Documentation"
+    }
+  end
+
+  test "generator run with PATCH" do
+    BlueBird.ConnLogger.reset()
+
+    # Create a test connection
+    conn = conn(:patch, "/patch", %{p: 5})
+    |> put_req_header("content-type", "application/json")
+
+    # Invoke the plug
+    conn = TestRouter.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 201
+
+    BlueBird.ConnLogger.save(conn)
+
+    assert BlueBird.Generator.run() ==
+    %{description: "Enter API description in mix.exs - blue_bird_info",
+      host: "http://localhost",
+      routes: [
+        %{description: nil,
+          group: "Test",
+          method: "GET",
+          note: nil,
+          parameters: [],
+          path: "/get",
+          requests: [],
+          title: "Test GET"
+        },
+        %{description: nil,
+          group: "Test",
+          method: "POST",
+          note: "This is a note",
+          parameters: [],
+          path: "/post",
+          requests: [],
+          title: "Test POST"
+        },
+        %{description: nil,
+          group: "Test",
+          method: "PUT",
+          note: nil,
+          parameters: [],
+          path: "/put",
+          requests: [],
+          title: "Test PUT"
+        },
+        %{description: nil,
+          group: "Test",
+          method: "PATCH",
+          note: nil,
+          parameters: [
+            %{description: "Post ID or slug",
+              name: "post_id",
+              required: true,
+              type: "integer"
+            }
+          ],
+          path: "/patch",
+          requests: [
+            %{headers: [{"content-type", "application/json"}],
+              method: "PATCH",
+              params: "{\"p\":5}",
+              path: "/patch",
+              path_params: %{},
+              response: %{
+                body: "{\"status\":\"ok\"}",
+                headers: [{"cache-control", "max-age=0, private, must-revalidate"}],
+                status: 201
+              }
+            }
+          ],
+          title: "Test PATCH"
+        },
+        %{description: nil,
+          group: "Test",
+          method: "DELETE",
+          note: nil,
+          parameters: [],
+          path: "/delete",
+          requests: [],
+          title: "Test DELETE"
+        }
+      ],
+      title: "API Documentation"
+    }
+  end
+
+  test "generator run with DELETE" do
+    BlueBird.ConnLogger.reset()
+
+    # Create a test connection
+    conn = conn(:delete, "/delete", %{p: 5})
+    |> put_req_header("content-type", "application/json")
+
+    # Invoke the plug
+    conn = TestRouter.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 204
+
+    BlueBird.ConnLogger.save(conn)
+
+    assert BlueBird.Generator.run() ==
+    %{description: "Enter API description in mix.exs - blue_bird_info",
+      host: "http://localhost",
+      routes: [
+        %{description: nil,
+          group: "Test",
+          method: "GET",
+          note: nil,
+          parameters: [],
+          path: "/get",
+          requests: [],
+          title: "Test GET"
+        },
+        %{description: nil,
+          group: "Test",
+          method: "POST",
+          note: "This is a note",
+          parameters: [],
+          path: "/post",
+          requests: [],
+          title: "Test POST"
+        },
+        %{description: nil,
+          group: "Test",
+          method: "PUT",
+          note: nil,
+          parameters: [],
+          path: "/put",
+          requests: [],
+          title: "Test PUT"
+        },
+        %{description: nil,
+          group: "Test",
+          method: "PATCH",
+          note: nil,
+          parameters: [
+            %{description: "Post ID or slug",
+              name: "post_id",
+              required: true,
+              type: "integer"
+            }
+          ],
+          path: "/patch",
+          requests: [],
+          title: "Test PATCH"
+        },
+        %{description: nil,
+          group: "Test",
+          method: "DELETE",
+          note: nil,
+          parameters: [],
+          path: "/delete",
+          requests: [
+            %{headers: [{"content-type", "application/json"}],
+              method: "DELETE",
+              params: "{\"p\":5}",
+              path: "/delete",
+              path_params: %{},
+              response: %{
+                body: "{\"status\":\"ok\"}",
+                headers: [{"cache-control", "max-age=0, private, must-revalidate"}],
+                status: 204
+              }
+            }
+          ],
+          title: "Test DELETE"
+        }
+      ],
+      title: "API Documentation"
+    }
+  end
 end
