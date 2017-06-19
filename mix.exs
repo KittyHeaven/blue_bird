6d5f6d52814f3d0678891fb2ae4d8cf2d71113be
woylie <potoroo@posteo.de>
2017-06-19 09:41:18 +0200

    bump version, update changelog

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule BlueBird.Mixfile do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.3.2"
   @url "https://github.com/KittyHeaven/blue_bird"
   @maintainers [
     "Djordje Atlialp",
