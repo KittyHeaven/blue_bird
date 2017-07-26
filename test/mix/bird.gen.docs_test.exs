61ea46807e96e085bebbb50fcdc8532fc4255cc8
woylie <potoroo@posteo.de>
2017-07-26 21:36:35 +0200

    add bird.gen.docs test

---
+++ test/mix/bird.gen.docs_test.exs
@@ -0,0 +1,17 @@
+defmodule BlueBird.Test.Mix.BirdGenDocsTest do
+  use BlueBird.Test.Support.ConnCase
+
+  import Mix.Tasks.Bird.Gen.Docs
+
+  test "bird.gen.docs generates html file" do
+    path_apib = Path.join(["priv", "static", "docs", "index.html"])
+
+    File.rm(path_apib)
+    refute File.exists?(path_apib)
+
+    run(nil)
+
+    assert {:ok, file} = File.read(path_apib)
+    assert file =~ "<title>Heavenly API</title>"
+  end
+end
