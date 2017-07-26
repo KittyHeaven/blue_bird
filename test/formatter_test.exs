30dd4f197384a381427f3246de499db752a0f738
woylie <potoroo@posteo.de>
2017-07-26 20:43:11 +0200

    add formatter test

---
+++ test/formatter_test.exs
@@ -0,0 +1,26 @@
+defmodule BlueBird.Test.FormatterTest do
+  use ExUnit.Case
+
+  alias BlueBird.Formatter
+  alias BlueBird.Test.Support.Examples
+
+  test "Formatter runs Generator and Writer" do
+    path_apib = Path.join(["priv", "static", "docs", "api.apib"])
+    path_swagger = Path.join(["priv", "static", "docs", "swagger.json"])
+
+    File.rm(path_apib)
+    File.rm(path_swagger)
+
+    refute File.exists?(path_apib)
+    refute File.exists?(path_swagger)
+
+    assert Formatter.handle_event({:suite_finished, 1, 2}, nil) ==
+      :remove_handler
+
+    assert {:ok, file} = File.read(path_apib)
+    assert file =~ "HOST: https://justiceisusefulwhenmoneyisuseless.fake"
+
+    assert {:ok, file} = File.read(path_swagger)
+    assert file =~ "\"host\":\"justiceisusefulwhenmoneyisuseless.fake\""
+  end
+end
