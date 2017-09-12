7b5118564b8c4e4ead7b3cef1b12b4ac1d6019ab
woylie <potoroo@posteo.de>
2017-09-13 01:20:00 +0200

    fix mix task test

--- test/mix/bird_gen_docs_test.exs
+++ test/mix/bird_gen_docs_test.exs
@@ -4,6 +4,7 @@ defmodule BlueBird.Test.Mix.BirdGenDocsTest do
   import Mix.Tasks.Bird.Gen.Docs
 
   alias BlueBird.ApiDoc
+  alias BlueBird.Test.Support.Examples.NoRoutes
 
   test "bird.gen.docs generates html file" do
     path_apib = Path.join(["priv", "static", "docs", "index.html"])
@@ -11,7 +12,11 @@ defmodule BlueBird.Test.Mix.BirdGenDocsTest do
     File.rm(path_apib)
     refute File.exists?(path_apib)
 
-    run(%ApiDoc{})
+    # generate apib file
+    BlueBird.Writer.run(NoRoutes.api_doc())
+
+    # run mix task
+    run(nil)
 
     assert {:ok, file} = File.read(path_apib)
     assert file =~ "<title>Heavenly API</title>"
