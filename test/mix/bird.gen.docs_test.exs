443581cfb35cea2583a951f29832e23320892cfa
woylie <potoroo@posteo.de>
2017-09-12 21:34:22 +0200

    update test

--- test/mix/bird.gen.docs_test.exs
+++ test/mix/bird.gen.docs_test.exs
@@ -3,13 +3,15 @@ defmodule BlueBird.Test.Mix.BirdGenDocsTest do
 
   import Mix.Tasks.Bird.Gen.Docs
 
+  alias BlueBird.ApiDoc
+
   test "bird.gen.docs generates html file" do
     path_apib = Path.join(["priv", "static", "docs", "index.html"])
 
     File.rm(path_apib)
     refute File.exists?(path_apib)
 
-    run(nil)
+    run(%ApiDoc{})
 
     assert {:ok, file} = File.read(path_apib)
     assert file =~ "<title>Heavenly API</title>"
