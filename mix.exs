375846642bd2cc52a6f1c7df87cda2a25973b79a
Djordje Atlialp <djordje@atlialp.com>
2019-11-30 11:29:25 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule BlueBird.Mixfile do
   use Mix.Project
 
-  @version "0.4.1"
+  @version "0.4.2"
   @url "https://github.com/KittyHeaven/blue_bird"
   @maintainers [
     "Djordje Atlialp",
